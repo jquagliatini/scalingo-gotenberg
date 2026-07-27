51d2be42e0288d9db8cc936089322c8272845563
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2026-07-27 12:53:58 +0200

    fix: adds hyphen data

--- .profile.d/gotenberg.sh
+++ .profile.d/gotenberg.sh
@@ -3,3 +3,9 @@
 # Le paquet google-chrome-stable installé par le buildpack APT le pose ici :
 
 export CHROMIUM_BIN_PATH="/app/.apt/opt/google/chrome/google-chrome"
+
+# Dossier des données d'hyphénation Chromium, téléchargé au build par
+# bin/go-post-compile depuis le repo gotenberg upstream. Chromium cherche
+# ensuite `<dir>/<chromium-version>/hyph-<lang>.hyb`.
+
+export CHROMIUM_HYPHEN_DATA_DIR_PATH="/app/chromium-hyphen-data"
